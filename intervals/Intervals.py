13cf05f0bb6f252e547bd36f4fa15b72e1801d18
Angelo-D-Gomez <55332361+Angelo-D-Gomez@users.noreply.github.com>
2021-02-16 18:02:37 -0600

    added new assignment

---
+++ intervals/Intervals.py
@@ -0,0 +1,29 @@
+# Input: tuples_list is an unsorted list of tuples denoting intervals
+# Output: a list of merged tuples sorted by the lower number of the
+#         interval
+def merge_tuples (tuples_list):
+  pass
+
+# Input: tuples_list is a list of tuples of denoting intervals
+# Output: a list of tuples sorted by ascending order of the size of
+#         the interval
+#         if two intervals have the size then it will sort by the
+#         lower number in the interval
+def sort_by_interval_size (tuples_list):
+  pass
+
+
+def main():
+  # read the input data and create a list of tuples
+
+  # merge the list of tuples
+
+  # print the merged list
+
+  # sort the list of tuples according to the size of the interval
+
+  # print the sorted list
+
+
+if __name__ == "__main__":
+  main()
